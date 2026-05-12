d1758cd7cd706a9abd711512e971841f2eb802c1
Miguel Felipe Peñuela Garzón <felipegarxon@hotmail.com>
2026-05-12 15:59:51 -0500

    update register form

--- src/app/features/register/register.ts
+++ src/app/features/register/register.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from "@angular/router";
+import { Authentication } from '../../core/services/authentication';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    ReactiveFormsModule
+  ],
   templateUrl: './register.html',
   styleUrl: './register.scss',
+  standalone: true
 })
 export class Register {
+
+  form: FormGroup;
+  formBuilder: FormBuilder = inject(FormBuilder);
+
+  authenticationService: Authentication = inject(Authentication);
+
+  constructor() {
+    this.form = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      surname: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.requiredTrue]],
+      acceptTerms: ['', [Validators.requiredTrue]]
+    });
+
+    this.form.valueChanges.subscribe((values) => {
+      console.log('errors: ', this.form.errors);
+    })
+
+  }
+
+  validateConfirmPassword() {
+
+  }
+
+  createAccount() {
+
+  }
 
 }
