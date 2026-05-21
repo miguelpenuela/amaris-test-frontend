f0a9f01659646b242bd45cf2dae6a255135c3dc2
Miguel Felipe Peñuela Garzón <felipegarxon@hotmail.com>
2026-05-21 10:27:20 -0500

    update base path

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    basePath: 'http://3.84.173.144:8000/v1/api'
+    basePath: 'https://ypft69xdf0.execute-api.us-east-1.amazonaws.com/amaris-api-test/v1/api'
 };
